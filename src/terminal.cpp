6fb3a68ab693fe13c9f633645309fc34d7f3f483
fyang016 <fyang016@ucr.edu>
2015-11-20 11:18:58 -0800

    Update to assn2

--- src/terminal.cpp
+++ src/terminal.cpp
@@ -11,7 +11,7 @@
 #include <typeinfo>
 
 
-using namespace std;	
+using namespace std;
 using namespace boost;
 
 Terminal::Terminal(){
@@ -150,23 +150,24 @@ void Terminal::run(){
 	int exit = 0;
 	
 	// Extra credit part: store the login name and host name to output later
-	char *loginName = getlogin();
-	char hostName[128];
-	gethostname(hostName, sizeof hostName);
+	//char *loginName = getlogin();
+	//char hostName[128];
+	//gethostname(hostName, sizeof hostName);
 	
 	while (!exit){
 		
 		input = ""; //Erase the input4
 		string noComments;
 		while(input == ""){
-			cout << loginName << '@' << hostName << this->prompt << " ";
+			//cout << loginName << '@' << hostName << this->prompt << " ";
+			cout << this->prompt << " ";
 			getline(cin, input);
 			noComments =  this->throwAwayComments(input);
 			input = noComments;
-			if(bugSearch(input)){
-				cout << "Bug found in input" << endl;
-				input = "";
-			}
+			// if(bugSearch(input)){
+			// 	cout << "Bug found in input" << endl;
+			// 	input = "";
+			// }
 		}
 		
 		
@@ -174,22 +175,33 @@ void Terminal::run(){
 	    //Separat string into its induvidual parts and compare boolean values
 	    
 	    //Separate string by ; first
+    
+		int lastOperation = 0; //zero for OR one for AND
+		int result = 0;
+			
 		char_separator<char> separatorSemiColon(";");
 		tokenizer<char_separator<char> > tok(noComments, separatorSemiColon);
 		for(tokenizer<char_separator<char> >::iterator beg1=tok.begin(); beg1!=tok.end();++beg1){
-		
+			if(*beg1 == "("){
+	    		cout << "Open Parenthesis Found" << endl;
+	    		continue;
+	    	}
+	    	else if (*beg1 == ")"){
+	    		cout << "Close Parenthesis Found" << endl;
+	    		continue;
+	    	}
+    	
 			//Separate strings by || first do to presidance
 			char_separator<char> separatorAnd("&&");
 			tokenizer<char_separator<char> > tok(*beg1, separatorAnd);
-			
-			int lastOperation = 0; //zero for OR one for AND
-			int result = 0;
 			for(tokenizer<char_separator<char> >::iterator beg2=tok.begin(); beg2!=tok.end();++beg2){
+				
+				// cout << "Executing: " << *beg2 << endl;
+				// cout << "Results:" << endl;
 				
 				//Separate Strings by && and execute the commands
 				char_separator<char> separatorAnd("||");
 				tokenizer<char_separator<char> > tok(*beg2, separatorAnd);
-				
 				for(tokenizer<char_separator<char> >::iterator beg3=tok.begin(); beg3!=tok.end();++beg3){
 				
 					//If the result of the exe function is zero it means
@@ -207,10 +219,11 @@ void Terminal::run(){
 						}
 					}
 					lastOperation = 0;
-			}
+				}
 				lastOperation = 1;
 			}
 	 	}
+		
 	}	
 }
 
